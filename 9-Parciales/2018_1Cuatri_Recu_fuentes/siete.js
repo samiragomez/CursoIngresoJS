9ade42434a08c57eb59645c5fd179799d02869b2
samiragomez <40763634+samiragomez@users.noreply.github.com>
2018-07-30 12:29:12 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -1,4 +1,81 @@
-function mostrar()
+/*
+ingreso :
+maria f 15
+jose m 33
+pepe m 83
+mostrar cantidad
+   de mayores de edad = 2
+   menores de edad = 1 
+   mujeres = 1
+   cantidad de hombres= 2
+  nombre
+      del mayor = pepe
+      del menor = maria
+      del hombre de menor edad = jose
+*/     
+ function mostrar()
 {
+	var edad;
+	var nombre;
+	var sexo;
+	var contador;
+	var MayoresDeEdad;
+	var MenoresDeEdad;
+	var CantidadDeMujeres;
+	var CantidadDeHombres;	
+
+	contador= 0 ;
+	while( contador <3)
+	{
+		contador ++ ;
+		nombre = prompt("ing nombre");
+		sexo= prompt("ing sexo");
+		while( sexo != "f" && sexo != "m")
+		{
+			sexo = prompt("error");
+		}
+		
+
+
+
+		edad= prompt("ing edad ");
+		edad= parseInt(edad);
+
+		while ( edad <0 || edad >100)
+		{
+			edad= prompt("error");
+			edad= parseInt(edad);
+
+		}
+
+		if (edad > 18)
+		{
+			MayoresDeEdad++;
+		}
+		else 
+		{
+			MenoresDeEdad++;
+		}
+		if (sexo = "f")
+		{
+			CantidadDeMujeres++ ; 
+		}
+		else
+		{
+			CantidadDeHombres++;
+		}
+
+
+	
+     }
+     document.write(" <br> la cantidad de mayores es : " + MayoresDeEdad );
+     document.write(" <br> la cantidad de menores es : " + MenoresDeEdad);
+
+
+
+	
+
+
+
 
 }
